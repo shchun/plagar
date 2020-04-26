a713cd327007019d0f41eb34524964ffc90e86fb
Seunghyo Chun <seunghyo.chun@gmail.com>
2020-04-27 01:11:28 +0900

    update

--- user.cpp
+++ user.cpp
@@ -3,17 +3,18 @@
 #define NULL 0
 #endif 
  
-// #include <stdio.h>
+#include <stdio.h>
 
 #define MAX_USER 10000
 #define MAX_LEN 100
 #define MAX_WORD 8
 
-char data[MAX_USER][MAX_LEN];
+unsigned long long data[MAX_USER][MAX_LEN];
 char dataLength[MAX_USER];
 
 void initUser(int dataN){
     // printf("initUser: %d\n", dataN);
+    //printf("initUser: %d\n", sizeof(long));
     for(int i =0; i <= dataN; i++) {
         for (int j = 0; j < MAX_LEN; j++) {
             data[i][j] = 0;
@@ -26,68 +27,50 @@ void initUser(int dataN){
 void registerData(int dataID, int dataLen, int dataArr[100]){
     // printf("registerData: %d %d\n", dataID, dataLen);
 
-    dataLength[dataID] = dataLen;
+    int i = 0;
+    unsigned long long tempdata = 0;
+    for(i = 0; i < dataLen; i++) {
+        unsigned char tmpchar = (char) dataArr[i] + 128;
+        
+        //printf("tempdata 1 [%d][%llx] ch[%d] dataLen[%d]\n", i, tempdata, tmpchar, dataLen);
+        tempdata = ((tempdata << 8) | tmpchar );
+        //printf("tempdata 2 [%d][%llx] ch[%d]\n", i, tempdata, tmpchar);
 
-    for(int i = 0; i < dataLen; i++) {
-        data[dataID][i] = (char) dataArr[i];
+        if(i < 7) continue;
+        data[dataID][i-7] = tempdata; 
     }
+
+    dataLength[dataID] = dataLen -7;
+
 }
  
 int findID(int args[8])
 {
     int id  = 1;
+    unsigned long long stick = 0;
+
+    //printf("findID 1\n");
+    for(int i = 0; i < 8; i++) {
+        unsigned char tmpchar = (char) args[i] + 128;
+        stick = ((stick << 8) | tmpchar ); // & 0xFFFFFFFFFFFFFFFF; 
+    } 
+
+    int found = 0;
+    for(; id < MAX_USER; id++) {
+        
+        //printf("findID 2: id[%d] dataLen[%d]\n", id, dataLength[id]);
     
-    char chargs[8] = {0,};
-    for(int i=0; i < 8; i++) {
-        chargs[i] = (char) args[i];
-    }
-
-    do {
-            int dcur=0;
-            int found;
-                
-            while(dcur < dataLength[id]) {
+        for(int i =0; i < dataLength[id]; i++) {
+            if(stick == data[id][i]) { 
                 found = 1;
-                int acur = 0;
-                
-                if (dataLength[id] - dcur < MAX_WORD) {
-                    found = 0;
-                    break;
-                }
-
-                if(data[id][dcur] == chargs[acur]) {
-                    //printf("match ==> %d, %d\n", id, dcur);
-
-                    for (int i=1; i < MAX_WORD; i++) {
-                        if(data[id][dcur+i] != chargs[i]) {
-                            // not match
-                            //printf("not match ==> %d, %d, %d\n", id, dcur, i);
-
-                            found = 0;
-                            break;
-                        }
-                        //printf("i ==> [%d]\n", i);
-
-                    }
-                    if(found == 1) {
-                        //printf("break 1\n");
-                        break;
-                    } 
-
-                }
-                //if(found == 1) break;
-                dcur++;
-
-            }
-            if(found == 1) {
-                //printf("break 2\n");
                 break;
             }
-        
-    } while (id++);
-    /* printf("findID: %d, arg[%d, %d, %d, %d, %d, %d, %d, %d] dl[%d]\n", 
-        id, chargs[0], chargs[1], chargs[2], chargs[3], 
-        chargs[4], chargs[5], chargs[6], chargs[7], dataLength[id]);
-     */
+        }
+        if(found == 1) break;
+    } 
+    
+
+    //printf("findID 3: id[%d] found[%d]\n", id, found);
+
     return id;
 }
